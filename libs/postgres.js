f365c267ba0643c35023ce6c09be4c4d3c819d4c
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2023-01-01 18:15:16 +0000

    Conectando el proyecto a PostreSQL

---
+++ libs/postgres.js
@@ -0,0 +1,10 @@
+const { client, Client } = require('pg');
+
+async function getConnection(){
+    const client = new Client('postgres://tmorsibm:eEk_rXXBl_ov3e9c7baVGuKk2zuJBPzW@snuffleupagus.db.elephantsql.com/tmorsibm');
+    await client.connect();
+    return client;
+}
+
+module.exports = getConnection;
+
