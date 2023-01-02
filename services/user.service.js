589f4821036faa33eaa45dd6a99c211bd829b90f
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2023-01-02 16:36:57 +0000

    Implementando CRUD con Sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -6,7 +6,8 @@ class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data)
+    return newUser;
   }
 
   async find() {
@@ -14,18 +15,21 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if(!user){
+      throw boom.notFound('User not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    return await user.update(changes);
   }
 
   async delete(id) {
-    return { id };
+    const user = await this.findOne(id);
+    return await user.destroy();
   }
 }
 
